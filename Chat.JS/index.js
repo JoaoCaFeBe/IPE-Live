8a6a80b9b4ec05193624dd0f42ab0c9c31c204d9
João Carlos Freire Belian <60831590+JoaoCaFeBe@users.noreply.github.com>
2026-02-28 21:58:41 -0300

    Adicionar URL de origem para o CORS no servidor Socket.IO

--- Chat.JS/index.js
+++ Chat.JS/index.js
@@ -1,6 +1,6 @@
 const { Server } = require("socket.io");
 
-const io = new Server({ cors: { origin: ["http://transmissao", "http://localhost", "http://localhost:8080", "http://192.168.1.43", "http://10.0.0.253"] } });
+const io = new Server({ cors: { origin: ["http://transmissao", "http://localhost", "http://localhost:8080", "http://192.168.1.43", "http://10.0.0.253", "https://ipe.live.test"] } });
 
 io.on("connection", (socket) => {
     console.log("Nova conexão : " + socket.id);
